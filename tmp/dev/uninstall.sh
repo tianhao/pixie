b6ca3e2e3de4bf99e59a69fe0e9688b1af3f2c84
tianhao <tianhaolsk@qq.com>
2022-11-07 02:09:36 +0800

    开发环境

---
+++ tmp/dev/uninstall.sh
@@ -0,0 +1,14 @@
+
+
+
+kubectl get all -n plc-dev -o yaml > plc-dev-all.yaml
+kubectl get all -n plc-dev-monitoring -o yaml > plc-dev-monitoring-all.yaml
+kubectl get all -n pl-dev -o yaml > pl-dev-all.yaml
+
+kubectl delete -f plc-dev-all.yaml
+kubectl delete -f plc-dev-monitoring-all.yaml
+kubectl delete -f pl-dev-all.yaml
+
+kubectl delete namespace plc-dev
+kubectl delete namespace plc-dev-monitoring
+kubectl delete namespace pl-dev
